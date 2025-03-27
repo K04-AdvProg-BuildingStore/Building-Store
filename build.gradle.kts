8b9ef3f55b5dc2d61c2c564b0c70d82375b214f1
ZakiyDaulay <zakiydaulay@gmail.com>
2025-03-27 09:36:39 +0700

    sonar cloud analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "4.4.1.3373"
 }
+
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
@@ -36,4 +38,12 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "K04-AdvProg-BuildingStore_Building-Store")
+        property("sonar.organization", "k04-advprog-buildingstore")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
