518c7dffc60822a0717dd4a904b74846456944d1
unknown <flori.andrea.60@gmail.com>
2025-04-01 12:56:13 +0700

    [GREEN] Implemented AuthenticationController and AuthenticationService.

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/auth/controller/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.buildingstore.auth.controller;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    String password;
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/auth/controller/RegisterRequest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.buildingstore.auth.controller;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+}
