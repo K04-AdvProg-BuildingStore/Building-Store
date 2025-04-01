b31e73ed8aa83a1ba3dc17a948dcbdc901f5a187
unknown <flori.andrea.60@gmail.com>
2025-04-01 09:52:57 +0700

    [GREEN] Initial setup for build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,12 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation ("org.postgresql:postgresql")
-
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // JJWT (for JWT token handling)
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<Test> {
