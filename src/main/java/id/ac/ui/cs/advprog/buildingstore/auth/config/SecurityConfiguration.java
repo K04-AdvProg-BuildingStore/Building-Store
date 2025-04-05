8bf4b1b297d3e33df7d0549796c06be55c070c12
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-04-05 23:26:55 +0700

    [GREEN] Implemented User Role Authorization

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/auth/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/auth/config/SecurityConfiguration.java
@@ -1,9 +1,9 @@
 package id.ac.ui.cs.advprog.buildingstore.auth.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,8 +11,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
--- src/main/java/id/ac/ui/cs/advprog/buildingstore/demo/controller/DemoController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.buildingstore.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,23 @@ public class DemoController {
     @GetMapping
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hello World!");
+    }
+
+    @GetMapping("/cashier")
+    @PreAuthorize("hasRole('CASHIER')")
+    public ResponseEntity<String> cashierOnly() {
+        return ResponseEntity.ok("Cashier WORKS LESGOOO!!!!");
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> adminOnly() {
+        return ResponseEntity.ok("Admin WORKS LEGOOOO!!!!");
+    }
+
+    @GetMapping("/shared")
+    @PreAuthorize("hasAnyRole('CASHIER', 'ADMIN')")
+    public ResponseEntity<String> sharedAccess() {
+        return ResponseEntity.ok("BOTH ADMIN AND CASHIER WORKS FOR THIS ONE FUNCTION, YAYAYAYAYAYAY");
     }
 }
