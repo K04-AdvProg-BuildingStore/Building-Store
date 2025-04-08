317642b5224415f56247368e0419150ed3926bb6
Mcflurrins <flori.andrea.60@gmail.com>
2025-04-08 20:08:58 +0700

    [GREEN] Implemented Token Cleaning, the token database is scheduled to clean every midnight now

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/BuildingStoreApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/BuildingStoreApplication.java
@@ -3,8 +3,9 @@ package id.ac.ui.cs.advprog.buildingstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
+@EnableScheduling
 public class BuildingStoreApplication {
 
     public static void main(String[] args) {
