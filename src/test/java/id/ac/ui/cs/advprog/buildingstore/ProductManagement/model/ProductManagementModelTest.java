5a04f9c8cc165609482e66380806028734f88a87
ZakiyDaulay <zakiydaulay@gmail.com>
2025-04-09 20:51:07 +0700

    [RED] added test cases for product management model

---
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/ProductManagement/model/ProductManagementModelTest.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.buildingstore.ProductManagement.model;
+import id.ac.ui.cs.advprog.buildingstore.auth.model.User;
+import id.ac.ui.cs.advprog.buildingstore.auth.model.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagementModelTest{
+    @BeforeEach
+    void setUp() {
+        adminUser = User.builder()
+                .id(1)
+                .username("admin123")
+                .password("password")
+                .role(Role.ADMIN)
+                .build();
+
+        product = ProductModel.builder()
+                .name("Soda")
+                .quantity(10)
+                .price(1000)
+                .administrator(adminUser)
+                .status("Available")
+                .information("Amazing Incredible Yummy Soda")
+                .build();
+
+    }
+    @Test
+    void testProductInitialization(){
+        assertEquals("Soda", product.getName());
+        assertEquals(10, product.getQuantity());
+        assertEquals(1000, product.getPrice());
+
+        assertNotNull(product.getAdministrator());
+        assertEquals(1, product.getAdministrator().getId());
+        assertEquals("admin123", product.getAdministrator().getUsername());
+        assertEquals(Role.ADMIN, product.getAdministrator().getRole());
+
+        assertEquals("Available", product.getStatus());
+        assertEquals("Amazing Incredible Yummy Soda", product.getInformation());
+    }
+    @Test
+    void testUpdateStock() {
+        product.setQuantity(25);
+        assertEquals(25, product.getQuantity());
+    }
+
+    @Test
+    void testChangeStatus() {
+        product.setStatus("Discontinued");
+        assertEquals("Discontinued", product.getStatus());
+    }
+}
