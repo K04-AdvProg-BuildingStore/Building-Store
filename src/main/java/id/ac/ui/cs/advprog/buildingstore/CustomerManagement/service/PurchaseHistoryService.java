94b055018dc80a6fd10828e89874ea661c0ca8b9
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-04-10 15:37:04 +0700

    [REFACTOR] update the purchasehostory service to be both service and serviceIMPL

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/CustomerManagement/service/PurchaseHistoryService.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/CustomerManagement/service/PurchaseHistoryService.java
@@ -1,9 +1,10 @@
 package id.ac.ui.cs.advprog.buildingstore.CustomerManagement.service;
-
+import org.springframework.stereotype.Service;
 import id.ac.ui.cs.advprog.buildingstore.CustomerManagement.model.PurchaseHistoryModel;
 import id.ac.ui.cs.advprog.buildingstore.CustomerManagement.repository.PurchaseHistoryRepository;
 import java.util.List;
 
+@Service
 public class PurchaseHistoryService {
     private final PurchaseHistoryRepository respository;
 
