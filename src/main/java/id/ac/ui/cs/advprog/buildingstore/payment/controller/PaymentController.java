64c1ea112e8eac0e81d3abdedd2a12563f35cdc1
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-04-11 20:17:34 +0700

    [GREEN] Implemented Payment Controller Class

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/payment/controller/PaymentController.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.buildingstore.payment.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import id.ac.ui.cs.advprog.buildingstore.payment.model.Payment;
+import id.ac.ui.cs.advprog.buildingstore.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+        return ResponseEntity.ok(paymentService.create(payment));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Payment>> getAllPayments() {
+        return ResponseEntity.ok(paymentService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Payment> getPayment(@PathVariable UUID id) {
+        return ResponseEntity.ok(paymentService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Payment> updatePayment(@PathVariable UUID id, @RequestBody Payment payment) {
+        return ResponseEntity.ok(paymentService.update(id, payment));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePayment(@PathVariable UUID id) {
+        paymentService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
