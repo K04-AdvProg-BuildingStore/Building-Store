09f6d06a34f4ed40d373395e840002914f816b03
Mcflurrins <flori.andrea.60@gmail.com>
2025-04-11 16:22:04 +0700

    [GREEN] Implemented SalesItemRepository.

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/salesTransaction/repository/SalesItemRepository.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.buildingstore.salesTransaction.repository;
+
+import id.ac.ui.cs.advprog.buildingstore.salesTransaction.model.SalesItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SalesItemRepository extends JpaRepository<SalesItem, String> {
+}
