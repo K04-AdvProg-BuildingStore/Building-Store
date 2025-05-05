9b41345c1fdc58e73d08685240dcf847cd29301f
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-05-05 14:57:57 +0700

    [REFACTOR] Fixing create payment with error handling

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/payment/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/payment/service/PaymentServiceImpl.java
@@ -4,21 +4,25 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import id.ac.ui.cs.advprog.buildingstore.payment.model.Payment;
 import id.ac.ui.cs.advprog.buildingstore.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class PaymentServiceImpl implements PaymentService {
 
+    private final Logger log = LoggerFactory.getLogger(PaymentServiceImpl.class);
     private final PaymentRepository paymentRepository;
 
     @Override
     public Payment create(Payment payment) {
+        if (payment == null) {
+            throw new IllegalArgumentException("Payment cannot be null");
+        }
         // Using template method pattern
         return processPayment(payment);
     }
