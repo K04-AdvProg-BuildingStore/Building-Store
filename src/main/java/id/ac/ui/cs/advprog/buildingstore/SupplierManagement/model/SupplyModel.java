5aeed29d8e8b67653d57c121508931098699d034
terserahdehh <serafina.nala@gmail.com>
2025-05-09 14:47:49 +0700

    [GREEN] added SupplyModel

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/SupplierManagement/model/SupplyModel.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/SupplierManagement/model/SupplyModel.java
@@ -1,29 +1,34 @@
 package id.ac.ui.cs.advprog.buildingstore.SupplierManagement.model;
+
+//import id.ac.ui.cs.advprog.buildingstore.ProductManagement.model.ProductManagementModel;
 import jakarta.persistence.*;
 import lombok.*;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "supply")
-
 public class SupplyModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "supplied_product", nullable = false)
-    private String suppliedProduct;
+    // link back to the supplier
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "supplier_id", nullable = false)
+    private SupplierManagementModel supplier;
 
-    @Column(name = "phone_number", unique = true, nullable = false)
-    private String phoneNumber;
+    // link to the product being supplied
+    //@ManyToOne(optional = false)
+    //@JoinColumn(name = "product_id", nullable = false)
+    //private ProductManagementModel product;
 
-    @Column(name = "supplyStock", nullable = false)
+    @Column(name = "supply_stock", nullable = false)
     private Integer supplyStock;
 
     @Column(name = "delivery_address", nullable = false)
-    private String address;
-
+    private String deliveryAddress;
 }
