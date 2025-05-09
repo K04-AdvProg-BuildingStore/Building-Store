f3c4073087240d48cc96f33ab34035473d659c14
terserahdehh <serafina.nala@gmail.com>
2025-05-09 14:54:19 +0700

    [GREEN] added SupplyRepository

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/SupplierManagement/repository/SupplyRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/SupplierManagement/repository/SupplyRepository.java
@@ -1,4 +1,13 @@
 package id.ac.ui.cs.advprog.buildingstore.SupplierManagement.repository;
 
-public class SupplyRepository {
+import id.ac.ui.cs.advprog.buildingstore.SupplierManagement.model.SupplyModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SupplyRepository extends JpaRepository<SupplyModel, Integer> {
+    List<SupplyModel> findBySupplierId(Integer supplierId);
+  //  List<SupplyModel> findByProductId(Integer productId);
 }
